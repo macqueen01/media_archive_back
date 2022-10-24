d1138c73b3b67f971d373e97fb8e50206569a2cf
mocking jae <macqueen1001@gmail.com>
2022-10-24 16:28:30 +0900

    several model creation error mended -still needs some refactoring

--- django_backend/sample_backend/admin.py
+++ django_backend/sample_backend/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from .models import User, Case
+from .models import User, Case, Location, Personel, VideoCase, ImageCase, DocCase, VideoMedia, ImageMedia, DocMedia
 
 # Register your models here.
 
 admin.site.register(User)
 admin.site.register(Case)
+admin.site.register(Personel)
+admin.site.register(VideoCase)
+admin.site.register(VideoMedia)
+admin.site.register(Location)
+
 
 
--- django_backend/sample_backend/model/MediaModel.py
+++ django_backend/sample_backend/model/MediaModel.py
@@ -3,19 +3,19 @@ from . import abstractModels
 from . import CaseModel
 
 class VideoMedia(abstractModels.AbstractVideoMedia):
-    referenced_in = models.ManyToManyField(to = CaseModel.VideoCase, related_name = "include")
-    profile_of = models.ManyToManyField(to = CaseModel.Personel, related_name = "profile")
-    image_of = models.ManyToManyField(to = CaseModel.Location, related_name = "view")
+    referenced_in = models.ManyToManyField(CaseModel.VideoCase, related_name = "include")
+    profile_of = models.ManyToManyField(CaseModel.Personel, related_name = "profile")
+    image_of = models.ManyToManyField(CaseModel.Location, related_name = "view")
     class Meta:
         abstract = False
 
 class ImageMedia(abstractModels.AbstractImageMedia):
-    referenced_in = models.ManyToManyField(to = CaseModel.ImageCase, related_name = "include")
+    referenced_in = models.ManyToManyField(CaseModel.ImageCase, related_name = "include")
     class Meta:
         abstract = False
 
 class DocMedia(abstractModels.AbstractDocumentMedia):
-    referenced_in = models.ManyToManyField(to = CaseModel.DocCase, related_name = "include")
+    referenced_in = models.ManyToManyField(CaseModel.DocCase, related_name = "include")
     class Meta:
         abstract = False
 
