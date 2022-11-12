abbc7f7a73cdec014b3b24fed987198eed55e2f9
root <root@DESKTOP-7NMM00O.localdomain>
2022-11-12 16:50:01 +0900

    send message implementation in progress in message model manager

--- django_backend/sample_backend/model/MessageModel.py
+++ django_backend/sample_backend/model/MessageModel.py
@@ -1,6 +1,7 @@
 from . import UserModel
 from . import RequestModel
 from django.db import models
+from django.utils import timezone
 
 
 class MessageManager(models.Manager):
@@ -8,7 +9,9 @@ class MessageManager(models.Manager):
 
     def delete_message(self, message_obj):
         # this hides the message from the receiver
-        pass
+        message_obj.hidden = 1
+        message_obj.save()
+        return True
 
     def get_rejections(self):
         pass
@@ -16,14 +19,24 @@ class MessageManager(models.Manager):
     def get_admissions(self):
         pass
 
-    def get_messages_from_user(self, user_id):
-        pass
+    def get_messages_related_user(self, user_id):
+        q = moidels.Q()
+        q.add(models.Q(hidden__exact = 0), q.AND)
+        q.add(models.Q(receiver__id__exact = user_id) | 
+            models.Q(sender__id__exact = user_id), q.AND)
+        return self.filter(q)
 
     def get_messages_received_by_user(self, user_id):
-        pass
+        q = moidels.Q()
+        q.add(models.Q(hidden__exact = 0), q.AND)
+        q.add(models.Q(receiver__id__exact = user_id), q.AND)
+        return self.filter(q)
 
     def get_messages_sent_by_user(self, user_id):
-        pass
+        q = moidels.Q()
+        q.add(models.Q(hidden__exact = 0), q.AND)
+        q.add(models.Q(sender__id__exact = user_id), q.AND)
+        return self.filter(q)
 
     def send(self, sender, receiver_set, content, message_type = 2):
         # message_type == 2 -> the most normal and simple message type, no connected actions taken
@@ -31,7 +44,32 @@ class MessageManager(models.Manager):
         # message_type == 0 -> this should indicate rejection of a request. connected to the request object through foreign key
         # postpone -> this method should be implemented for both single receiver argument and multiple receivers argument.
         
-        pass
+
+        def create_message(receiver):
+            new_message = self.model(
+                sender = sender,
+                receiver = receiver,
+                content = content,
+                created_at = timezone.now()
+                message_type = message_type
+            )
+            new_message.save()
+            return new_message
+
+        is_queryset = isinstance(receiver_set, models.query.QuerySet)
+        message_list = []
+
+        if not is_queryset:
+            receiver = receiver_set
+            new_message = create_message(receiver)
+            message_list.append(new_message)
+            return message_list
+
+        for receiver in receiver_set:
+            new_message = create_message(receiver)
+            message_list.append(new_message)
+        
+        return message_list
 
     def visible_messages(self, receiver):
         # messages that are not deleted
@@ -41,6 +79,7 @@ class MessageModel(models.Model):
     sender = models.ForeignKey(UserModel.User, on_delete = models.SET_NULL, null = True, related_name = "message_sent")
     receiver = models.ForeignKey(UserModel.User, on_delete = models.SET_NULL, null = True, related_name = "message_received")
     content = models.TextField()
+    hidden = models.IntegerField(default = 0)
     created_at = models.DateTimeField()
     message_type = models.IntegerField()
 
